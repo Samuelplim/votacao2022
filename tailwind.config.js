22e28b2fb42f7eacaf40ca470a967b99492528e9
Samuelplim <samuel12nerd@gmail.com>
2022-10-17 01:53:31 -0400

    Adição do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
